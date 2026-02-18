ba6c670ad644225f2982432b749febcd54aca3b7
EL HAMDOAUI Omar <omarelhamdaoui39@gmail.com>
2026-02-18 10:58:16 +0100

    chore: delete DSC/v2

--- DSC/v2/Modules/MyDscConfigurationState/MyDscConfigurationState.psm1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem -Path "$PSScriptRoot\*.ps1" -Exclude '*.Tests.ps1' | ForEach-Object { . $_.FullName }
-
-Export-ModuleMember `
-    -Function Get-DscConfigurationState, Test-DscConfigurationState, Set-DscConfigurationState, Compare-DscConfigurationState
