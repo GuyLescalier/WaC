4b9d449c556745ffab9dc5f94aeff27f8a620406
EL HAMDOAUI Omar <omarelhamdaoui39@gmail.com>
2026-04-17 10:30:58 +0200

    fix: requested changes

--- DSC/Modules/MyResources/MyScoopPackage/MyScoopPackage.ps1
+++ DSC/Modules/MyResources/MyScoopPackage/MyScoopPackage.ps1
@@ -10,13 +10,9 @@ function Assert-ScoopIsInstalled {
     }
 }
 
-function Test-GitInstalled {
-    try {
-        Get-Command git -ErrorAction Stop | Out-Null
-        return $true
-    }
-    catch {
-        return $false
+function Ensure-GitInstalled {
+    if ($null -eq (Get-Command git -ErrorAction SilentlyContinue)) {
+        scoop "install" git
     }
 }
 
@@ -27,13 +23,11 @@ function Get-LatestVersion {
         [string]$pkgName
     )
 
-    if (-not (Test-GitInstalled)) {
-        scoop install git
-    }
+    Ensure-GitInstalled
+   
+    scoop "update"
 
-    scoop update
-
-    $manifest = scoop cat $pkgName | ConvertFrom-Json
+    $manifest = scoop "cat" $pkgName | ConvertFrom-Json
     $version = $manifest.version
 
     return $version    
@@ -42,61 +36,48 @@ function Get-LatestVersion {
 function Get-ResourceState {
     param($InputObject)
 
-    Assert-ScoopIsInstalled
-
     $pkgName = $InputObject.packageName
 
     $pkgInfo = scoop "info" $pkgName 6>$null
 
     if ($null -eq $pkgInfo.Name) {
-        throw "Could not find manifest for '$PkgName' in local buckets."
+        throw "Could not find manifest for '$pkgName' in local buckets."
     }
 
 
-    if ($null -ne ($pkgInfo.Installed) ) {
+    if ($null -eq $pkgInfo.Installed) {
 
-        if ($InputObject.version -eq 'latest') {
-            $version = Get-LatestVersion -pkgName $pkgName
-        }
-        else {
-            $version = $InputObject.version
-        }
-
-        $state = if ($pkgInfo.Installed -eq $version) { 'Present' } else { 'Stale' }
-
-        return @{
-            packageName      = $pkgName
-            ensure           = $state
-            version          = $InputObject.version
-            installedVersion = $pkgInfo.Installed
-        }
-    }
-    else {
         return @{
             packageName = $pkgName
             ensure      = 'Absent'
         }
+    }
+
+    if ($InputObject.version -eq 'latest') {
+        $version = Get-LatestVersion -pkgName $pkgName
+    }
+    else {
+        $version = $InputObject.version
+    }
+
+    $state = if ($pkgInfo.Installed -eq $version) { 'Present' } else { 'Stale' }
+
+    return @{
+        packageName      = $pkgName
+        ensure           = $state
+        version          = $InputObject.version
+        installedVersion = $pkgInfo.Installed
     }
 }
 
 function Test-ResourceState {
     param($InputObject)
 
-    Assert-ScoopIsInstalled
-
     $currentState = Get-ResourceState -InputObject $InputObject
     $desiredEnsure = $InputObject.ensure
 
-    $inDesired = $false
+    $currentState._inDesiredState = ($currentState.ensure -eq $desiredEnsure)
 
-    if ($desiredEnsure -eq 'Present') {
-        $inDesired = ($currentState.ensure -eq 'Present')
-    }
-    elseif ($desiredEnsure -eq 'Absent') {
-        $inDesired = ($currentState.ensure -eq 'Absent')
-    }
-
-    $currentState._inDesiredState = $inDesired
     return $currentState
 }
 
@@ -104,40 +85,39 @@ function Test-ResourceState {
 function Set-ResourceState {
     param($InputObject)
 
-    Assert-ScoopIsInstalled
-
     $pkgName = $InputObject.packageName
 
-    if ($InputObject.ensure -eq 'Present') {
+    if ($InputObject.ensure -eq 'Absent') {
+        scoop "uninstall" $pkgName
+        return
+    }
         
-        $currentState = Get-ResourceState -InputObject $InputObject
+    $currentState = Get-ResourceState -InputObject $InputObject
 
-        if ($InputObject.version -eq 'latest') {
-            $version = Get-LatestVersion -pkgName $pkgName
-        }
-        else {
-            $version = $InputObject.version
-        }
-
-        $installArg = if ($version -ne 'latest') { "$pkgName@$version" } else { $pkgName }
-
-        if ($currentState.ensure -eq 'Absent') {
-            scoop "install" $installArg
-        }
-        else {
-            if (-not (Test-GitInstalled)) {
-                scoop install git
-            }
-            scoop "update" $installArg
-        }
-
+    if ($InputObject.version -eq 'latest') {
+        $version = Get-LatestVersion -pkgName $pkgName
     }
     else {
-        scoop "uninstall" $pkgName
+        $version = $InputObject.version
     }
+
+    $installArg = if ($version -ne 'latest') { "$pkgName@$version" } else { $pkgName }
+
+    if ($currentState.ensure -eq 'Absent') {
+        scoop "install" $installArg
+    }
+    else {
+        Ensure-GitInstalled
+
+        scoop "update" $installArg
+    }
+
+
 }
 
 try {
+    Assert-ScoopIsInstalled
+
     $inputJson = [Console]::In.ReadToEnd()
     $inputObject = $inputJson | ConvertFrom-Json
 
