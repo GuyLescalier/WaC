13f580c497eab7e2360b49e00e0df7343af1e665
EL HAMDOAUI Omar <omarelhamdaoui39@gmail.com>
2026-08-18 11:10:17 +0200

    fix:  uninstall resources with Set-ResourceState

--- DSC/Modules/MyResources/MyChocolateyPackage/MyChocolateyPackage.ps1
+++ DSC/Modules/MyResources/MyChocolateyPackage/MyChocolateyPackage.ps1
@@ -95,22 +95,21 @@ function Test-ResourceState {
 }
 
 function Set-ResourceState {
-    param($InputObject)
+    param($InputObject)
+
+    $pkgName = $InputObject.packageName
 
     if ($InputObject.ensure -eq 'Absent') {
-
-        & choco uninstall $target -y
+        & choco uninstall $pkgName -y
         return
     }
 
-    $target = $InputObject.packageName
-
-    if ($InputObject.version -ne 'latest') {
-        $target += " --version $($InputObject.Version)"
+    if ($InputObject.version -eq 'latest') {
+        & choco upgrade $pkgName -y
     }
-    # If you do not have a package installed, upgrade will install it.
-    & choco upgrade $target -y
-
+    else {
+        & choco upgrade $pkgName "--version=$($InputObject.version)" -y
+    }
 }
 
 try {
